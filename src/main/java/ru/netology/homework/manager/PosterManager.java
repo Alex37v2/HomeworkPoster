987a38b7eb1892781fb1dedd7b62e7a8e65f7fd5
Алексей Александров <Alex37v2@gmail.com>
2022-09-24 00:20:26 +1000

    adding movies and manager

---
+++ src/main/java/ru/netology/homework/manager/PosterManager.java
@@ -0,0 +1,48 @@
+package ru.netology.homework.manager;
+
+import lombok.Data;
+import ru.netology.homework.domain.Movie;
+
+@Data
+public class PosterManager {
+    private Movie[] movies = new Movie[0];
+    private int defaultMovieLength = 10;
+
+
+    PosterManager() {
+
+    }
+
+    public PosterManager(int customMovieLength) {
+        if (customMovieLength > 0) {
+            defaultMovieLength = customMovieLength;
+        }
+
+    }
+
+    public void addMovie(Movie movie) {
+        int length = movies.length + 1;
+        Movie[] tmp = new Movie[length];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        int lastMovie = tmp.length - 1;
+        tmp[lastMovie] = movie;
+        movies = tmp;
+
+    }
+
+    public Movie[] getLastAdd() {
+        int moviesLength = movies.length;
+        if (moviesLength < defaultMovieLength) {
+            defaultMovieLength = moviesLength;
+        }
+        Movie[] customFilm = new Movie[defaultMovieLength];
+        for (int i = 0; i < customFilm.length; i++) {
+            int result = moviesLength - i - 1;
+            customFilm[i] = movies[result];
+
+        }
+        return customFilm;
+    }
+}
